769ef1031d1bf38e3b2f7052936ce6d12412ba75
Mikhail <mixicpro@gmail.com>
2023-09-19 20:11:14 +0300

    Added docker and README

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("fileSystem/", include("fileSystem.urls")),
+    path("", include("fileSystem.urls")),
 ]
